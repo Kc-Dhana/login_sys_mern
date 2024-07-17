a08b14d3f15aea9943ef46850a56bb1081684dc2
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-07-17 17:04:42 +0530

    Design login Ui & add fonts

--- login_app_mern/client/src/components/Username.js
+++ login_app_mern/client/src/components/Username.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import avatar from '../assets/profile.png';
+
+import styles from '../styles/Username.module.css';
 
 export default function Username() {
   return (
     <div className="container mx-auto">
-      <div className="flex r h-screen">
-        <div>
+      <div className="flex justify-center items-center h-[105vh]">
+        <div className={styles.glass}>
 
           <div className="title flex flex-col items-center">
-            <h4 className='text-5xl font-bold'>Hello Again!</h4>
+            <h4 className='text-4xl font-bold'>Hello Again!</h4>
             <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
             Explore More by connecting with us.
             </span>
@@ -16,12 +19,12 @@ export default function Username() {
 
           <form className='py-1'>
             <div className='profile flex justify-center py-4'>
-              <img src="" alt="avatar"/>
+              <img src={avatar} className={styles.profile_img} alt="avatar"/>
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
-              <input type="text" placeholder='Username' />
-              <button type='submit'>Let's Go</button>
+              <input className={styles.textbox} type="text" placeholder='Username' />
+              <button className={styles.btn} type='submit'>Let's Go</button>
             </div>
 
             <div className="text-center py-4">
