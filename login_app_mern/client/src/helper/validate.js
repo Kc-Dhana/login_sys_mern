626d91e44ff5341ff64670d755b7ea055320c408
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-07-17 17:40:04 +0530

    validate username

--- login_app_mern/client/src/helper/validate.js
+++ login_app_mern/client/src/helper/validate.js
@@ -1,3 +1,8 @@
-
+import toast from "react-hot-toast"
 
 /**validate username */
+function usernameVerify(error = {},values){
+    if(!values.username){
+        error.username=toast.error('Username Required...!');
+    }
+}
